0d78b0ce12a4b0d5d0e6abb7870a04c0e8958e11
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-03-12 22:29:29 -0600

    Se agrego el Linked

---
+++ Linked.java
@@ -0,0 +1,85 @@
+import java.util.LinkedHashMap;
+import java.util.Set;
+import java.util.Map.Entry;
+/**
+ * @author DIEDERICH SOLIS
+
+ */
+public class Linked<K,V> implements InterfaceMap<K,V> {
+	LinkedHashMap <K,V> map = new LinkedHashMap<K,V>();
+	/** En donde:
+	* @param k  LlaveInterface
+	* @param v  Valor
+	**/
+
+	/**
+	*@pre: Con la ayuda de un map se obtiene el tamaño
+	*@post: Se obtiene el tamaño del map
+	*/
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return map.size();
+	}
+	/**
+	*@pre: Se cuenta con una llave y un valor
+	*@post: Se asigna un valor a la llave
+	*/
+	@Override
+	public void put(K k, V v) {
+		// TODO Auto-generated method stub
+		map.put(k,v);
+	}
+	/**
+	*@pre: La llave existe
+	*@post: Retorna el valor asignado a la llave
+	*/	
+	@Override
+	public V get(K k) {
+		// TODO Auto-generated method stub
+		return map.get(k);
+	}
+	/**
+	*@pre: La llave existe
+	*@post: Se remueve el valor de la llave y la llave, ademas de que retorna el valor removido
+	*/
+	@Override
+	public V remove(K k) {
+		// TODO Auto-generated method stub
+		return map.remove(k);
+	}
+	/**
+	*@pre: La llave existe
+	*@post: Retorna verdadero si la llave se encuentra en el mapa
+	*/	
+	@Override
+	public boolean containsKey(K k) {
+		// TODO Auto-generated method stub
+		if (map.get(k)!=null) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	/**
+	*@pre: El valor existe
+	*@post: Retorna verdadero si el valor se encuentra en el mapa
+	*/	
+	@Override
+	public boolean containsValue(V v) {
+		// TODO Auto-generated method stub
+		if (map.containsValue(v)) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return map.entrySet();
+	}
+}
